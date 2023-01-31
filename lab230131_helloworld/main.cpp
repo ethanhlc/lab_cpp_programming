a9b45641c2417ae7f3d7f762c0afe35934b7f2f2
Ethan Baik <sakithree@gmail.com>
2023-01-31 10:50:57 +0900

    Consecutive input test

--- lab230131_helloworld/main.cpp
+++ lab230131_helloworld/main.cpp
@@ -5,10 +5,8 @@ int main(void)
     int val1, val2, val3;
     int result;
 
-    std::cout << "1st input: ";
-    std::cin >> val1;
-    std::cout << "2nd input: ";
-    std::cin >> val2;
+    std::cout << "1st & 2nd input: ";
+    std::cin >> val1 >> val2;
     std::cout << "3rd input: ";
     std::cin >> val3;
 
