99b31cbda9b78f101869f138543d8cc29778035f
Ethan Baik <sakithree@gmail.com>
2023-01-31 10:36:01 +0900

    Hello world

---
+++ lab230131_helloworld/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+    int num = 20;
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Foo" << std::endl << ">> Bar" << std::endl;
+    std::cout << "num = " << num;
+    std::cout << " pi : " << 3.14 << std::endl;
+
+    return 0;
+}
