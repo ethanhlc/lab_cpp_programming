1dcb60fbd62731e2a7f3a7a71ea3626eb1ab2588
Ethan Baik <sakithree@gmail.com>
2023-02-14 15:50:39 +0900

    return *this to allow chaining of operations

--- lab230214/ch10.cpp
+++ lab230214/ch10.cpp
@@ -6,25 +6,29 @@ namespace mystd
     class ostream
     {
     public:
-        void operator<< (char *str)
+        ostream &operator<< (char *str)
         {
             printf("%s", str);
+            return *this;
         }
-        void operator<< (char str)
+        ostream &operator<< (char str)
         {
             printf("%c", str);
+            return *this;
         }
-        void operator<< (int num)
+        ostream &operator<< (int num)
         {
             printf("%d", num);
+            return *this;
         }
-        void operator<< (double e)
+        ostream &operator<< (double e)
         {
             printf("%g", e);
+            return *this;
         }
-        void operator<< (ostream &(*fp)(ostream &ostm))
+        ostream &operator<< (ostream &(*fp)(ostream &ostm))
         {
-            fp(*this);
+            return fp(*this);
         }
     };
 
@@ -43,12 +47,7 @@ int main(void)
     using mystd::cout;
     using mystd::endl;
 
-    cout << "Simple String";
-    cout << endl;
-    cout << 3.14;
-    cout << endl;
-    cout << 123;
-    endl(cout);
+    cout << "Simple String" << endl << 123 << endl << 3.14 << endl;
 
     return 0;
 }
