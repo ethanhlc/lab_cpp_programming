514f37eaed9f7409eb3d1c302055b78b15417b8e
Ethan Baik <sakithree@gmail.com>
2023-02-16 11:11:31 +0900

    Template ex2 automatic type selection

--- lab230216/ch13_templates.cpp
+++ lab230216/ch13_templates.cpp
@@ -10,10 +10,10 @@ T Add(T num1, T num2)
 
 int main(void)
 {
-    cout << Add<int>(15, 20) << endl;
-    cout << Add<double>(2.9, 3.7) << endl;
-    cout << Add<int>(3.2, 3.2) << endl;
-    cout << Add<double>(3.14, 2.75) << endl;
+    cout << Add(15, 20) << endl;
+    cout << Add(2.9, 3.7) << endl;
+    cout << Add(3.2, 3.2) << endl;
+    cout << Add(3.14, 2.75) << endl;
 
     return 0;
 }
