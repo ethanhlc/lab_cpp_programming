391c8ae25290f54167f0ca1ff6bc9e5cf094ab01
Ethan Baik <sakithree@gmail.com>
2023-02-28 10:49:53 +0900

    Init 0228 MFC messages

---
+++ lab230228/lab230228Dlg.h
@@ -0,0 +1,34 @@
+
+// lab230228Dlg.h : header file
+//
+
+#pragma once
+
+
+// Clab230228Dlg dialog
+class Clab230228Dlg : public CDialogEx
+{
+// Construction
+public:
+	Clab230228Dlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LAB230228_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
