48aa285030ecd62b3be150e1a0d8632d52f1dd7d
Ethan Baik <sakithree@gmail.com>
2023-02-28 15:34:50 +0900

    MFC_Stopwatch start/stop/reset

---
+++ lab230228Stopwatch/lab230228StopwatchDlg.h
@@ -0,0 +1,45 @@
+
+// lab230228StopwatchDlg.h : header file
+//
+
+#pragma once
+
+
+// Clab230228StopwatchDlg dialog
+class Clab230228StopwatchDlg : public CDialogEx
+{
+// Construction
+public:
+	Clab230228StopwatchDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LAB230228STOPWATCH_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnDestroy();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	CString m_sTimer;
+private:
+	bool m_bRun;
+	bool m_bReset;
+};
